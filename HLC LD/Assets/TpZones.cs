7610b9ecd143bf3c1cb0a535909184cc07551116
enzoprog <e.ferreiradealmeida@rubika-edu.com>
2023-05-07 21:02:10 +0200

    Un peu d'UI

---
+++ HLC LD/Assets/TpZones.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TpZones : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private GameObject canvas;
+
+    [SerializeField] private KeyCode tp = KeyCode.F;
+
+    private void OnTriggerStay(Collider other)
+    {
+        canvas.SetActive(true);
+        
+        if (Input.GetKeyDown(tp))
+        {
+            player.transform.position = target.transform.position;
+        }
+    }
+}
